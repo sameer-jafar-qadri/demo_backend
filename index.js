f860f9e9e6d8c3448647b4071dbfe50dcf3466b3
sameer-jafar-qadri <sameer.jafar.qadri@gmail.com>
2026-04-09 23:44:26 +0530

    first commit

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ app.get("/image/:id", (req, res) => {
     });
 });
 
+app.get('/',(req,res)=>{
+    res.send("hello render")
+})
 // IMPORTANT for Render:
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
